4f10f797716d7d7734be5589a02e81610e612f50
Michael <90275701+Michael-Zagon@users.noreply.github.com>
2024-10-31 16:09:20 -0400

    Add files via upload

---
+++ busi1400_helloworld.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Zagon
+# Created on: Oct 2024
+# This is my first python code for BUSI1400
+
+print("Hello World!")
+print("BUSI1400")
+
+# More than one arguments in the print function
+print("Hello", "Python")
+
+# Print numbers and strings
+print("The result of 3+2 is:", 3+2)
+
+# Print 7
+print(3+4)
+
+# print Hello World in two lines
+print("Hello")
+print("World")
+
+# print multiple values with a single print function 
+print("My favourite numbers are:", 3+5, "and", 3+3)
+
+# print three lines including a blank line
+print("Goodbye")
+print()
+print("Have a good day!")
+
+# print values with variable
+myNum_1 = 3+5
+myNum_2 = 3+3
+
+print("My favourite numbers are ", myNum_1, "and", myNum_2)
+
+
+print("\nDone")
